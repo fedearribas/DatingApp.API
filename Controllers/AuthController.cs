8d2e6084da183983ff7174a283b2fbea01e69c73
Federico Arribas <fede.arribas@gmail.com>
2018-04-24 16:31:50 -0300

    Added Authentication

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,76 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _configuration;
+        public AuthController(IAuthRepository repo, IConfiguration configuration)
+        {
+            this._configuration = configuration;
+            this._repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody]UserForRegisterDto userDto)
+        {
+            userDto.Username = userDto.Username.ToLower();
+
+            if (await _repo.UserExists(userDto.Username))
+                ModelState.AddModelError("Username", "Username is already taken");
+
+            //validate request
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = new User
+            {
+                Username = userDto.Username
+            };
+            var createdUser = await _repo.Register(user, userDto.Password);
+            return StatusCode(201);
+        }
+
+        public async Task<IActionResult> Login([FromBody]UserForLoginDto userDto)
+        {
+            var userFromRepo = await _repo.Login(userDto.Username.ToLower(), userDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            // generate token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings.Token").Value);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                    new Claim(ClaimTypes.Name, userFromRepo.Username)
+                }),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { tokenString });
+
+        }
+    }
+}
